425d37d3c4106b867eb0ac03b6f69efa312053a9
pfzw90 <pfzw90@gmail.com>
2020-10-10 18:51:41 +0300

    changed uploading process

--- yadisk.py
+++ yadisk.py
@@ -7,18 +7,21 @@ class YaUploader:
     def upload(self, file_path: str):
         """Метод загруджает файл file_path на яндекс диск"""
 
+        file_path = os.path.normpath(file_path)
         HEADERS = {"Authorization" : f'OAuth {self.token}'}
+        FILES = {"file" : open(file_path, 'rb')}
 
         response_url = requests.get(
         "https://cloud-api.yandex.net/v1/disk/resources/upload",
         params = {"path": file_path} ,
         headers = HEADERS)
-       
-        url = response_url.json()['href']
-        response_upload = requests.put(url)
-        return print(response_upload)
+        url = response_url.json().get('href')
+
+
+        response_upload = requests.put(url, files = FILES, headers = {})
+        return print(response_upload.status_code)
 
 
 if __name__ == '__main__':
     uploader = YaUploader('XXX')
-    result = uploader.upload('\files-to-upload\art5.jpg')
+    result = uploader.upload("files-to-upload/test/art10.jpg")
